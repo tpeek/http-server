39e2c8d4a0a032ce883f77416a10fae9f574b149
wesleywooten <wootenwesley@gmail.com>
2015-06-29 16:40:41 -0700

    server and client

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+ADDR = ("127.0.0.1", 8001)
+server = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
+)
+
+client = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
+)
+
+client.connect(ADDR)
+msg = "do you hear me? Cause I hear you. I am going to eat food"
+
+try:
+    client.sendall(msg)
+    client.shutdown(socket.SHUT_WR)
+    while True:
+        part = client.recv(16)
+        print part
+        if len(part) < 16:
+            client.close()
+            break
+except Exception as e:
+    print e
---
+++ http-server.py
@@ -0,0 +1,34 @@
+import socket
+
+ADDR = ("127.0.0.1", 8001)
+server = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
+)
+
+def response_ok():
+    return "HTTP/1.1 200 OK <html><head><title>Success!</title></head><body><header><h1>Success!</h1></header><p>It worked</p></body></html>"
+
+def response_error():
+    return "HTTP/1.1 500 Internal Server Error <html><head><title>Error!</title></head><body><header><h1>Error!</h1></header><p>It did not work</p></body></html>"
+
+server.bind(ADDR)
+server.listen(1)
+while True:
+    try:
+        conn, addr = server.accept()
+        s, msg= "",True
+        while msg:
+            msg = conn.recv(16)
+            s+=msg
+            if len(msg) < 16: 
+                conn.sendall(response_ok())
+                conn.close()
+                break
+        print s
+        
+    except KeyboardInterrupt as e:
+        break
+    except Exception as e:
+        print e
+        conn.sendall(response_error())
+        
